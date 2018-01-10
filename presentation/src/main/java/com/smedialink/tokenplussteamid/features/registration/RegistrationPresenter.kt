547f6b6ca8b2a57091548f4cf21a9effda0a0a69
Sergey Vasilchenko <svasilchenko@smedialink.com>
2018-01-10 11:58:48 +0300

    Add post-registration navigation

--- presentation/src/main/java/com/smedialink/tokenplussteamid/features/registration/RegistrationPresenter.kt
+++ presentation/src/main/java/com/smedialink/tokenplussteamid/features/registration/RegistrationPresenter.kt
@@ -2,14 +2,17 @@ package com.smedialink.tokenplussteamid.features.registration
 
 import com.arellomobile.mvp.InjectViewState
 import com.smedialink.tokenplussteamid.base.BasePresenter
+import com.smedialink.tokenplussteamid.features.MyAppScreens
 import com.smedialink.tokenplussteamid.interactor.RegistrationInteractor
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import ru.terrakok.cicerone.Router
 import timber.log.Timber
 import javax.inject.Inject
 
 @InjectViewState
 class RegistrationPresenter @Inject constructor(
+        private val router: Router,
         private val interactor: RegistrationInteractor
 ) : BasePresenter<RegistrationView>() {
 
@@ -20,9 +23,10 @@ class RegistrationPresenter @Inject constructor(
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
-                    Timber.d("Registration completed.")
+                    Timber.i("Registration completed.")
+                    router.navigateTo(MyAppScreens.STEAM_AUTH_SCREEN)
                 }, { throwable ->
-                    Timber.d("Registration error: ${throwable.message}")
+                    Timber.e("Registration error: ${throwable.message}")
                 })
                 .also { disposable -> unsubscribeOnDestroy(disposable) }
     }
